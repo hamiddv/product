7899f958b04c3ade5737614037507d0f39ce277d
hamiddv <hhossini399@gmail.com>
2023-06-04 17:58:16 +0330

    add email verify url

--- product_site/user/urls.py
+++ product_site/user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import create_user, login_user
+from .views import create_user, login_user, email_verify
 
 urlpatterns = [
     path('create-user/', create_user, name='create_user'),
     path('login/', login_user, name='login_user'),
+    path('verify-email/', email_verify, name='verify_email')
 ]
