ee9695e7d147622803329242418543c5c8395781
hamiddv <hhossini399@gmail.com>
2023-06-05 20:15:49 +0330

    debug

--- product_site/user/views.py
+++ product_site/user/views.py
@@ -30,7 +30,7 @@ def create_user(request):
 def create_code_verify_email(username):
     code = randint(100000, 999999)
     user = CustomUser.objects.get(username=username)
-    user.email_verification_code = code
+    user.email_verify_code = code
     user.save()
 
 
@@ -39,48 +39,67 @@ def email_verify(request):
     username = request.data['username']
     email = request.data['email']
     code = request.data['code']
+    print('code = ' , code)
     new_code = request.data['new_code']
 
     try:
         user = CustomUser.objects.get(username=username)
     except ObjectDoesNotExist:
-        return Response({'message': 'Invalid username'}, status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            {
+                'message': 'Invalid username'
+            },
+            status=status.HTTP_404_NOT_FOUND
+        )
 
+    if new_code == True:
+        create_code_verify_email(username)
+        print('real code' + user.email_verify_code)
+        # print('new code' + new_code)
+
+    # print('new_code' , user.email_verify_code)
     if code != False:
-        if user.email == email:
 
-            if new_code == True:
-                create_code_verify_email(username)
-                print(user.email_verify_code)
-            if code == user.email_verify_code:
-                user.is_email_verified = True
-                user.email_verify_code = None
-                user.save()
-                return Response(
-                    {
-                        'message': 'Verification code is valid'
-                    },print(user.email_verify_code)
-                    ,
-                    print(status=status.HTTP_200_OK)
-                )
-            else:
-                return Response(
-                    {
-                        'message': 'Invalid verification code'
-                    },print(user.email_verify_code)
-                    ,
-                    status=status.HTTP_400_BAD_REQUEST
-                )
+        if code == user.email_verify_code:
+            print(user.email_verify_code)
+            user.is_email_verified = True
+            user.email_verify_code = None
+            user.save()
+            return Response(
+                {
+                    'message': 'Verification code is valid'
+                },
+                status=status.HTTP_200_OK
+            )
 
+        else:
+            return Response(
+                {
+                    'message': 'Invalid verification code',
+
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
     elif code == False:
         if new_code == True:
             create_code_verify_email(username)
+            print(user.email_verify_code)
             return Response(
                 {
-                    'message' :'code creaetd'
-                }, user.email_verify_code
+                    'message': 'code created'
+                },
+                status=status.HTTP_200_OK
             )
+
+    return Response(
+        {
+            'message': 'Invalid request'
+        },
+        status=status.HTTP_400_BAD_REQUEST
+    )
+
+
 
 
 
